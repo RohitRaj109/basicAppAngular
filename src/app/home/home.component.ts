7c8ea18661379a0e7941cdcfca47ed0b281af85b
Rohit Raj Singh <rohitraj.smsit@gmail.com>
2023-05-31 22:12:09 +0530

    validation complitted

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ import { ServerService } from '../provider/server.service';
 })
 export class HomeComponent implements OnInit{
 form:FormGroup= this.myForm()
-  allData:any=[];
+  allData:any=[];submitted:boolean = false
 
 constructor(private fb:FormBuilder,public server:ServerService){
   
@@ -17,11 +17,11 @@ constructor(private fb:FormBuilder,public server:ServerService){
 myForm(){
   return this.fb.group({
  name:['',[Validators.required]],
- email:['',[]],
- phone:['',[]],
- userName:['',[]],
- password:['',[]],
- cPassword:['',[]]
+ email:['',[Validators.required]],
+ phone:['',[Validators.required]],
+ userName:['',[Validators.required]],
+ password:['',[Validators.required]],
+ cPassword:['',[Validators.required]]
   })
 }
 ngOnInit(){
@@ -31,6 +31,7 @@ get f(){
   return this.form.controls;
 }
   onSubmit(){
+    this.submitted = true;
     if(this.form.invalid && (this.form.value.passoword != this.form.value.cPassword)){
       return;
     }
@@ -43,14 +44,20 @@ get f(){
       password:this.form.value.password,
     
     }
-    this.allData.push(data)
+   
     this.server.postData('/register',data).subscribe((res:any)=>{
-     
       if(res.success ==true){
-        console.log(res)
+        this.allData.push(data)
+        console.log(data)
+        alert(res.message)
+        this.submitted = false;
+        this.form.reset();
+      }else if(res.success ==false){
         alert(res.message)
       }
-    })
-    console.log(data)
+    },(error)=>{
+      alert(error)
+      console.log(data)
+    })  
   }
 }
